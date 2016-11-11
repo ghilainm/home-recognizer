81f7e9d261fa4c0cb13ee99fd0168d59cda74ddd
Matthieu Ghilain <ghilainm@gmail.com>
2016-11-11 16:56:17 +0100

    Initialized project with spring boot

---
+++ src/main/java/com/home/recognizer/HomeRecognizerApplication.java
@@ -0,0 +1,12 @@
+package com.home.recognizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeRecognizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeRecognizerApplication.class, args);
+	}
+}
